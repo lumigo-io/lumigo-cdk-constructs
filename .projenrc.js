4b8e73941c93403ecf609161b1a8e41bd23f0c4c
Michele Mancioppi <michelem@lumigo.io>
2022-11-12 17:19:19 +0100

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Michele Mancioppi',
+  authorAddress: 'michelem@lumigo.io',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'lumigo-cdk-constructs',
+  repositoryUrl: 'https://github.com/michelem/lumigo-cdk-constructs.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
